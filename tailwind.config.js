ddc899e9b311e8598b37b3dc6251e77eef10197c
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-04-26 11:41:15 +0530

    starter pack

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    debugScreens: {
+      position: ["bottom", "left"],
+    },
+    screens: {
+			xxs: "300px",
+		    // => @media (min-width: 300px) { ... }
+      s: "475px",
+      // => @media (min-width: 475px) { ... }
+			xs: "390px",
+		    // => @media (min-width: 390px) { ... }
+            
+			sm: "640px",
+			// => @media (min-width: 640px) { ... }
+
+			md: "768px",
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		},
+    extend: {
+      width: {
+				'128': '32rem',
+			  },
+			height: {
+				'128': '32rem',
+			  },
+    },
+  },
+  plugins: [
+    
+  ],
+}
